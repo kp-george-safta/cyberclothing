3469a236a3b29447390e5406826c61034644760b
gedy208 <edygeorgescu2006@gmail.com>
2025-08-27 16:40:31 +0300

    deleted discord icon added checkout icon (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import Home from './pages/home';
 import Login from './pages/login';
+import About from './pages/about';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About/>}/>
       </Routes>
       <Footer/>
     </>
